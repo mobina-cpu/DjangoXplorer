ee43e0b36cde8549c469ae61b17d7d1188629cc9
mobina <mobbinate00@gmail.com>
2024-09-17 16:43:30 +0330

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+class HelloView(APIView):
+    def get(self, request):
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response ')
+            data = response.json()
+        except ConnectionError:
+            logger.critical('httpbin is offline')
+
+        return render(request, 'hello.html', {'name': 'Mobina'})
